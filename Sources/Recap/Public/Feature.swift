fa72d7a617c28bd9168b88b59cbe848457997895
Jeff Verkoeyen <jeff@clutch.engineering>
2024-10-15 10:29:21 -0700

    Add support for system colors.
    Closes https://github.com/mergesort/Recap/issues/7

--- Sources/Recap/Public/Feature.swift
+++ Sources/Recap/Public/Feature.swift
@@ -7,18 +7,24 @@ public struct Feature: Identifiable, Equatable {
     public let title: String
     public let description: String
     public let symbolName: String
-    public let hexColor: String
+    public let colorDescription: String
 
-    public init(title: String, description: String, symbolName: String, hexColor: String) {
+    public init(title: String, description: String, symbolName: String, colorDescription: String) {
         self.title = title
         self.description = description
         self.symbolName = symbolName
-        self.hexColor = hexColor
+        self.colorDescription = colorDescription
     }
 }
 
 internal extension Feature {
     var color: Color {
-        Color(hex: self.hexColor)
+        if self.colorDescription.hasPrefix("#") {
+            return Color(hex: self.colorDescription)
+        } else if let color = ColorName(rawValue: self.colorDescription.trimmingCharacters(in: .whitespaces).lowercased())?.asColor() {
+            return color
+        } else {
+            return .white
+        }
     }
 }
