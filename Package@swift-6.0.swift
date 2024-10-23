8ce1035505017e6d72a309c4b2c48ab6b79ecc20
Joe Fabisevich <joe@fabisevi.ch>
2024-10-22 23:00:22 -0400

    Adding Package@swift-6.0.swift

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Recap",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v16), .macCatalyst(.v16)
+    ],
+    products: [
+        .library(
+            name: "Recap",
+            targets: ["Recap"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Recap",
+            dependencies: [],
+            resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "RecapTests",
+            dependencies: ["Recap"]
+        )
+    ]
+)
